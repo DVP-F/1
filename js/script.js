41d2f79431bfbd77aaf274e5905ba3b6cb687577
DVP-F <dvpf@duck.com>
2024-09-04 17:53:09 +0200

    i hope this works UmU

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-//! RIP my guy fr
+//! idk if this works i cant check it rn
+var mode = True
+function modeTheme(){
+    const root = document.querySelector(':root');
+    if(mode==True){
+        mode = False;
+        root.style.setProperty('--main_bg-colour', '#ffffd0');
+        root.style.setProperty('--main_text-colour', '#000');
+        root.style.setProperty('--tshad_1', '2px 2px 1px #fff, 2px -2px 1px #fff, -2px -2px 1px #fff, -2px 2px 1px #fff');
+        root.style.setProperty('--tshad_2', '2px 2px 1px #000, 2px -2px 1px #000, -2px -2px 1px #000, -2px 2px 1px #000');
+        document.getElementById('modeTog').innerText = '☾';
+    }
+    else{
+        mode = True;
+        root.style.setProperty('--main_bg-colour', '#000015');
+        root.style.setProperty('--main_text-colour', '#fff');
+        root.style.setProperty('--tshad_1', '2px 2px 1px #000, 2px -2px 1px #000, -2px -2px 1px #000, -2px 2px 1px #000');
+        root.style.setProperty('--tshad_2', '2px 2px 1px #fff, 2px -2px 1px #fff, -2px -2px 1px #fff, -2px 2px 1px #fff');
+        document.getElementById('modeTog').innerText = '☉';
+    }
+}
