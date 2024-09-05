504fc2614c23303aa0c48e8c23568f0c6e26ceac
DVP-F <dvpf@duck.com>
2024-09-05 09:30:03 +0200

    finished

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
-//! idk if this works i cant check it rn
-var mode = True
+var mode = true
 function modeTheme(){
     const root = document.querySelector(':root');
-    if(mode==True){
-        mode = False;
-        root.style.setProperty('--main_bg-colour', '#ffffd0');
+    if(mode==true){
+        mode = false;
+        root.style.setProperty('--main_bg-colour', '#ffdadf');
         root.style.setProperty('--main_text-colour', '#000');
-        root.style.setProperty('--tshad_1', '2px 2px 1px #fff, 2px -2px 1px #fff, -2px -2px 1px #fff, -2px 2px 1px #fff');
+        root.style.setProperty('--main_nav-text-colour', '#fff')
+        root.style.setProperty('--tshad_1', '2px 2px 2px #fff, 2px -2px 2px #fff, -2px -2px 2px #fff, -2px 2px 2px #fff');
         root.style.setProperty('--tshad_2', '2px 2px 1px #000, 2px -2px 1px #000, -2px -2px 1px #000, -2px 2px 1px #000');
-        document.getElementById('modeTog').innerText = '☾';
+        document.getElementById('modeTog').innerText = '☾ ';
     }
     else{
-        mode = True;
-        root.style.setProperty('--main_bg-colour', '#000015');
+        mode = true;
+        root.style.setProperty('--main_bg-colour', '#000');
         root.style.setProperty('--main_text-colour', '#fff');
+        root.style.setProperty('--main_nav-text-colour', '#000')
         root.style.setProperty('--tshad_1', '2px 2px 1px #000, 2px -2px 1px #000, -2px -2px 1px #000, -2px 2px 1px #000');
         root.style.setProperty('--tshad_2', '2px 2px 1px #fff, 2px -2px 1px #fff, -2px -2px 1px #fff, -2px 2px 1px #fff');
         document.getElementById('modeTog').innerText = '☉';
